31cb6718507f9ad6ec574d41cd7b8b3e687283c3
Evan Busse <evanbusse@gmail.com>
2016-10-07 13:18:56 -0600

    Completed bonus Part2

--- part2/bonus.js
+++ part2/bonus.js
@@ -1,3 +1,4 @@
+"use strict";
 // Define a function named bigSum that takes in an arbitrary number of
 // arguments. Assume all arguments are a number.
 //
@@ -6,7 +7,14 @@
 //
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
 function bigSum() {
-  // YOUR CODE HERE
+  if (arguments.length === 0) {
+    return 0;
+  }
+  var sum = 0;
+  for(var x = 0; x < arguments.length; x++) {
+    sum += parseInt(arguments[x]);
+  }
+  return sum;
 }
 
 // Define a function named pluck that takes in two arguments.
@@ -24,8 +32,13 @@ function bigSum() {
 //
 // and the key 'name', then return ['moe', 'larry', 'curly'].
 
-
-
+function pluck (arr, key) {
+  var names = []
+  for(var x = 0; x < arr.length; x++) {
+    names.push(arr[x][key]);
+  }
+  return names;
+}
 
 // Define a function named pick that takes in two arguments.
 //    obj (object)
@@ -35,8 +48,13 @@ function bigSum() {
 // { name: 'moe', age: 50, id: 1 } and ['name', 'age'], then return
 // { name: 'moe', age: 50 }.
 
-
-
+function pick (obj, keys) {
+  var newObj = {}
+  for(var x = 0; x < keys.length; x++) {
+    newObj[keys[x]] = obj[keys[x]];
+  }
+  return newObj;
+}
 
 // Define a function named merge that takes in two arguments.
 //    arr1 (array of sorted numbers)
@@ -45,7 +63,11 @@ function bigSum() {
 // Return a new array that contains all the numbers in each array still sorted.
 // For example, given [1, 3, 5] and [2, 4], then return [1, 2, 3, 4, 5].
 
-
+function merge (arr1, arr2) {
+  arr1.push(arr2);
+  arr1 = [].concat.apply([], arr1);
+  return arr1.sort();
+}
 
 
 // Define a function named unique that takes in one argument.
@@ -54,3 +76,13 @@ function bigSum() {
 // Return a new array that contains all the elements of arr minus any
 // duplicates. For example, given ['a', 'b', 'a', 'c'], then return
 // ['a', 'b', 'c'].
+
+function unique (arr) {
+  var unique = [];
+  for(var x = 0; x < arr.length; x++) {
+    if(! unique.includes(arr[x])){
+      unique.push(arr[x]);
+    }
+  }
+  return unique;
+}
